60ce72d3755c7d7e67013524b908eba06ab6e241
Ben Rasmusen <mail@benrasmusen.com>
2011-12-06 13:21:01 -0800

    Add Remark fixture.

---
+++ tests/fixtures/remark_fixture.php
@@ -0,0 +1,36 @@
+<?php
+/* Remark Fixture generated on: 2011-12-06 14:12:40 : 1323205600 */
+class RemarkFixture extends CakeTestFixture {
+	var $name = 'Remark';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'foreign_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'title' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'content' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'is_deleted' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'model' => 'User',
+			'foreign_id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'user_id' => 1,
+			'parent_id' => 1,
+			'is_deleted' => 0,
+			'created' => '2011-12-06 14:06:40',
+			'modified' => '2011-12-06 14:06:40'
+		),
+	);
+}
+?>
